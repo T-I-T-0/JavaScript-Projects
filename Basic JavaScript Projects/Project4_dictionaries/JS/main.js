7b9bfee8d0560cc9ae27d4667ca9b75f779b0fbf
T-I-T-0 <mirzaei.mani@outlook.com>
2024-07-17 16:12:13 -0700

    Project4_dictionaries

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,12 @@
+function my_Dictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Bread:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    delete Animal.Sound; //remove the KVP from the Dictionary
+    
+    document.getElementById("Dictionary").innerHTML = Animal.Bread
+}
