842fb6e86e7378cfd6ee8ffbc9761493b710f0bd
T-I-T-0 <mirzaei.mani@outlook.com>
2024-07-18 21:09:04 -0700

    Project6_ternary_operators_constructors assignment

---
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,39 @@
+
+// Using a ternary operatiom with input from from browser
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+// Constructor function utilizing *new and *this keywords
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + 
+    " manufactured in " + Erik.Vehicle_Year;
+}
+// nested function
+function Nested_Function() {
+    document.getElementById("Nested_Function").innerHTML = count();
+    function count() {
+        var Starting_point = 9;
+        function plus_one() {Starting_point +=1;}
+        plus_one();
+        return Starting_point;
+    }
+}
+
+
+
+
